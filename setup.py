21b3d951d8a9304598f5bc04cbc3355cbe7f1883
Olivier Breuleux <breuleux@gmail.com>
2019-08-17 20:42:26 -0400

    Set the README content type

--- setup.py
+++ setup.py
@@ -12,10 +12,11 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='breakword',
-    version='0.1.1',
+    version='0.1.2',
 
     description='Mixing breakpoints with print debugging.',
     long_description=long_description,
+    long_description_content_type='text/markdown',
 
     url='https://github.com/breuleux/breakword',
 
